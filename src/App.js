ac01aef68a5dc78ff633f6165d747fdbb9718727
MianHannan <hannanchudri@gmail.com>
2021-09-29 18:38:37 +0500

    UseState Hook Practice Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export default function App() {
+  
+  const [counter, setCounter] = React.useState(0);
+
   return (
     <div>
       <h1>Use State Hook Practice</h1>
+      <h2>{"Counter Value is : "+counter}</h2>
+      <div>
+        <button onClick={()=>{setCounter(counter+1)}}>Increment</button>
+        <button onClick={()=>{setCounter(counter-1)}}>Decrement</button>
+      </div>
+
     </div>
   )
 }
