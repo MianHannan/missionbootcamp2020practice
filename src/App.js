4858659c3001285933f816e2d32c71a0800e7230
MianHannan <hannanchudri@gmail.com>
2021-09-29 19:56:21 +0500

    UseReducer Hook Practice1 complete

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React from 'react'
 
 export default function App() {
   
-  const [counter, setCounter] = React.useState(0);
+  const [counter, setCounter] = React.useReducer(
+    (state,action)=>{
+      switch(action.type){
+        case "increment":
+          return {count : state.count+action.value, show: state.show};
+        case "decrement":
+          return {count : state.count-action.value, show: state.show};
+        case "toggle":
+          return {count : state.count, show: !state.show};
+        default:
+          return state;
+      }
+    },{
+      count:0,
+      show:true
+    });
 
   return (
     <div>
-      <h1>Use State Hook Practice</h1>
-      <h2>{"Counter Value is : "+counter}</h2>
+      <h1>Use Reducer Hook Practice</h1>
+      <h2>Counter Value is : <span>{counter.show?counter.count:null}</span></h2>
       <div>
-        <button onClick={()=>{setCounter(counter+1)}}>Increment</button>
-        <button onClick={()=>{setCounter(counter-1)}}>Decrement</button>
+        <button onClick={()=>{setCounter({type:"decrement",value:5})}}>Decrement</button>
+        <button onClick={()=>{setCounter({type:"toggle"})}}>Toggle</button>
+        <button onClick={()=>{setCounter({type:"increment",value:5})}}>Increment</button>
       </div>
 
     </div>
