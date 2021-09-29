73552a3beb29bb6ffe5c90c7eb9d3ad2753f59c6
MianHannan <hannanchudri@gmail.com>
2021-09-29 20:47:04 +0500

    UseState Hook Practice2 Complete

--- src/App.js
+++ src/App.js
@@ -1,33 +1,71 @@
 import React from 'react'
 
 export default function App() {
-  
-  const [counter, setCounter] = React.useReducer(
-    (state,action)=>{
-      switch(action.type){
-        case "increment":
-          return {count : state.count+action.value, show: state.show};
-        case "decrement":
-          return {count : state.count-action.value, show: state.show};
-        case "toggle":
-          return {count : state.count, show: !state.show};
-        default:
-          return state;
-      }
-    },{
-      count:0,
-      show:true
-    });
+
+  const initialState = {
+    count : 0,
+    change : 1,
+    show : true
+  };
+
+  const reducer = (state,action)=>{
+    switch(action.type){
+      case "decrement":
+        return {
+          ...state,
+          count:state.count-state.change
+        };
+      case "increment":
+      return {
+        ...state,
+        count:state.count+state.change
+      };
+      case "change":
+        return {
+          ...state,
+          change:action.change
+        };
+      case "toggle":
+        return {
+          ...state,
+          show:!state.show
+        };
+      default:
+        return state;
+    }
+  }
+
+  const [counterState, changeCounter] = React.useReducer(
+    reducer,
+    initialState
+  );
 
   return (
     <div>
       <h1>Use Reducer Hook Practice</h1>
-      <h2>Counter Value is : <span>{counter.show?counter.count:null}</span></h2>
+      <h2>{"Counter Value is : "}
+        <span>{counterState.show?counterState.count:null}</span>
+      </h2>
+      <h2>{"Count Change is : "}
+        <input 
+          type='number' 
+          value={counterState.change} 
+          onChange={(event)=>{
+            changeCounter({
+              type:"change",
+              change:event.target.value*1
+              })}}
+          ></input>
+      </h2>
       <div>
-        <button onClick={()=>{setCounter({type:"decrement",value:5})}}>Decrement</button>
-        <button onClick={()=>{setCounter({type:"toggle"})}}>Toggle</button>
-        <button onClick={()=>{setCounter({type:"increment",value:5})}}>Increment</button>
+        <button onClick={()=>{changeCounter({type:"decrement"})}}>Decrement</button>
+        <button onClick={()=>{changeCounter({type:"toggle"})}}>Toggle</button>
+        <button onClick={()=>{changeCounter({type:"increment"})}}>Increment</button>
       </div>
+      <div>
+        <button onClick={()=>{console.log(counterState)}}>Console Log</button>
+      </div>
+
 
     </div>
   )
